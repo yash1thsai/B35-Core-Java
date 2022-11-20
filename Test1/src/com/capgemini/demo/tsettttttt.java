115b28bc9c9e14933cd2517f1997bce80b0c01a3
yash1th <yash1th77@gmail.com>
2022-11-20 12:29:51 +0530

    second commit

---
+++ Test1/src/com/capgemini/demo/tsettttttt.java
@@ -0,0 +1,12 @@
+package com.capgemini.demo;
+
+public class tsettttttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int s=5;
+		System.out.println("the num :"+s);
+
+	}
+
+}
