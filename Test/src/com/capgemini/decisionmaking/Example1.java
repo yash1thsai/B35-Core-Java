17aa951b7d35baa013d49d1fdc1cba1278e10b06
yash1th <yash1th77@gmail.com>
2022-11-21 11:14:06 +0530

    assignment

---
+++ Test/src/com/capgemini/decisionmaking/Example1.java
@@ -0,0 +1,16 @@
+package com.capgemini.decisionmaking;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=7,b=9;
+			if(a>b)
+			{
+				System.out.println(a);
+			}
+			else
+			{
+				System.out.println(b);
+			}
+		}
+}
