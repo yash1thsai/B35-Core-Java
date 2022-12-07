47461cdba435c3cd45e2094efeb14272e392f5a0
yash1th <yash1th77@gmail.com>
2022-12-07 19:14:49 +0530

    day-12

---
+++ Day-12_Arrays/src/com/cg/arraysdemo/ArrayExample.java
@@ -0,0 +1,16 @@
+package com.cg.arraysdemo;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String[] arr= {"January","February","March","April","May"};//String
+				//int arr[]= {12,4,6,9};//I
+				float arr[]= {12.6f,4.9f,69.4f};//F
+				/*getClass() Returns the runtime class of this Object*/
+				/*getName() Returns the canonical name of the underlying class asdefined by The Java Language Specification.*/
+				System.out.println(arr.getClass().getName());
+
+	}
+
+}
---
+++ Day-12_Arrays/src/com/cg/arraysdemo/Varargs.java
@@ -0,0 +1,22 @@
+package com.cg.arraysdemo;
+
+//Varargs Allows methods to receive unspecified number of argument
+public class Varargs {
+	//String ...s is variable -argument and it contain o-n values
+	public static void print(int a,int b,String ...s)
+	{
+		System.out.println(a+" "+b);
+		//to print varargs
+		for(int i=0;i<s.length;i++)
+		{
+			System.out.print(s[i]+"\t");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		//fun call
+		print(12,25,"Java","SpringBoot","HTML","CSS","JS");
+	}
+
+}
