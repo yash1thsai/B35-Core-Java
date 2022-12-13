a87362b6d90e5d881fbc4c64e48662c7fcda1804
yash1th <yash1th77@gmail.com>
2022-12-13 15:30:24 +0530

    day-14

---
+++ Day-14_Collection/src/com/cg/collectiondemo/StackDemo.java
@@ -0,0 +1,22 @@
+package com.cg.collectiondemo;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		Stack<String>obj=new Stack<>();
+		//insertion
+		obj.push("Shiwani");
+		obj.push("Pooja");
+		obj.push("Yogesh");
+		obj.push("Rajesh");
+		System.out.println(obj);
+		//remove-LIFO
+		obj.pop();
+		System.out.println(obj);
+
+
+	}
+
+}
