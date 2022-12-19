6c04695e934f4cd04cbb267dad102ea23a855d47
yash1th <yash1th77@gmail.com>
2022-12-19 19:05:45 +0530

    commit

---
+++ Day-19_MultiThreadingAndConcurrency/src/com/cg/threadsdemo/MultiThreadingDemo.java
@@ -0,0 +1,5 @@
+package com.cg.threadsdemo;
+
+public class MultiThreadingDemo {
+
+}
---
+++ Day-19_MultiThreadingAndConcurrency/src/com/cg/threadsdemo/SingleThreadDemo.java
@@ -0,0 +1,10 @@
+package com.cg.threadsdemo;
+
+public class SingleThreadDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
